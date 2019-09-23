6c2d20de3182e8da97da370f8bb1249bc7380fdb
pimenov01 <trololo80.ivi@gmail.com>
2019-09-23 19:57:43 +0300

    Исправил недочеты и сделал еще несколько задач.

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -2,7 +2,13 @@
 
 package lesson2.task2
 
+import junit.framework.Assert.assertFalse
+import junit.framework.Assert.assertTrue
 import lesson1.task1.sqr
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.sqrt
 
 /**
  * Пример
@@ -18,12 +24,9 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean {
-    return when {
-        number % 10 + (number / 10) % 10 == (number / 100) % 10 + number / 1000 -> true
-        else -> false
-    }
-}
+fun isNumberHappy(number: Int): Boolean =
+    number % 10 + (number / 10) % 10 == (number / 100) % 10 + number / 1000
+
 
 /**
  * Простая
@@ -32,7 +35,9 @@ fun isNumberHappy(number: Int): Boolean {
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean =
+
+    (x1 == x2) || (y1 == y2) || abs(x1 - x2) == abs(y1 - y2)
 
 
 /**
@@ -41,7 +46,16 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+    var february = 28
+    february += if (year % 4 != 0 || year % 100 == 0 && year % 400 != 0) 0 else 1
+    return when (month) {
+        1, 3, 5, 7, 8, 10, 12 -> 31
+        4, 6, 9, 11 -> 30
+        else -> february
+    }
+
+}
 
 /**
  * Средняя
@@ -53,7 +67,8 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean =
+    r2 - r1 >= sqrt(sqr(x1 - x2) + sqr(y1 - y2))
 
 /**
  * Средняя
